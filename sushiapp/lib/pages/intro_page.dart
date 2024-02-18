c92af1e37292855048f6b4881f9e1ef16a81f4af
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-02-18 20:01:05 +0530

    created flutter project

---
+++ sushiapp/lib/pages/intro_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class IntroPage extends StatelessWidget{
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
