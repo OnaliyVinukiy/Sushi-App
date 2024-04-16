3282d236be3c6fe3c0aecc51e3da02fdd1242357
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-04-16 15:59:53 +0530

    developed get started button for intro page

--- sushi/lib/components/button.dart
+++ sushi/lib/components/button.dart
@@ -2,25 +2,37 @@ import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
   final String text;
+  final void Function()? onTap;
   const MyButton({
     super.key,
     required this.text,
+    required this.onTap,
     });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Color.fromARGB(109, 140, 94,91)),
-        padding:EdgeInsets.all(20),
-        child: Row(
-          children: [
-            Text(
-              text,
-              style: TextStyle(color: Colors.white),
-            ),
-          ],
-        ),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color.fromARGB(109, 140, 94,91), borderRadius: BorderRadius.circular(40),),
+          padding:EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                text,
+                style: TextStyle(color: Colors.white),
+              ),
+      
+              const SizedBox(width: 10),
+              Icon(
+                Icons.arrow_forward,
+                color: Colors.white,
+              ),
+            ],
+          ),
+      ),
     );
   }
 }
